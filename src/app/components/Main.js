2c24cb549a12173262fc23259b59c60db43dc5fd
3vdi <unitcomics.world@proton.me>
2023-09-30 11:48:59 +0530

    Login & Signup form

---
+++ src/app/components/Main.js
@@ -0,0 +1,18 @@
+export default function Main() {
+    return (
+        <div>
+            <div className="... flex justify-center items-center text-8xl">
+                <h1>Ghibli-Stream</h1>
+            </div>
+            <div className="... flex justify-center items-center m-8">
+                <img src="https://cdn.dribbble.com/users/47000/screenshots/1472080/pavlov_ghibli_mashup_dribbbs.jpg?resize=768x576&vertical=center" 
+                    width={'400vw'} alt="ghibli" />
+            </div>
+            <div className="... flex justify-center text-3xl mb-10">
+                <button type="button" class="text-gray-900 bg-gradient-to-r from-red-200 via-blue-200 to-yellow-200
+   hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400
+   font-medium rounded-sm text-lg px-5 py-2.5 text-center mr-2 mb-2">start watching</button>
+            </div>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,11 @@
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/login";
 
 export default function Home() {
   return (
-    <div>
-    <div className="... flex justify-center items-center text-8xl">
-    <h1>Ghibli-Stream</h1>
-  </div>
-  <div className="... flex justify-center items-center m-8">
-    <img src="https://cdn.dribbble.com/users/47000/screenshots/1472080/pavlov_ghibli_mashup_dribbbs.jpg?resize=768x576&vertical=center" width={'400vw'}
-    alt="ghibli" />
-  </div>
-  <div className="... flex justify-center text-3xl mb-10">
-  <button type="button" class="text-gray-900 bg-gradient-to-r from-red-200 via-blue-200 to-yellow-200
-   hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400
-   font-medium rounded-sm text-lg px-5 py-2.5 text-center mr-2 mb-2">start watching</button>
-  </div>
-  </div>
+    // <Main/>
+    // <Login/>
+    <Signup/>
   )
 }
