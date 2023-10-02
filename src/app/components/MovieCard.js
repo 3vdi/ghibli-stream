fbed7fbee6b06bc492fb90362d4f35466661ba23
3vdi <unitcomics.world@proton.me>
2023-10-02 10:03:51 +0530

    db.js

--- src/app/components/MovieCard.js
+++ src/app/components/MovieCard.js
@@ -5,13 +5,6 @@ export default function MovieCard({ movie }) {
             <a href="#">
                 <img class="rounded-lg" src={movie.image} width={'250px'} alt="" />
             </a>
-
-            {/* <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{movie.title}</h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{movie.description}</p>
-            </div> */}
         </div>
     )
 }
