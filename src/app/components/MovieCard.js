5a9aaad60ed4c22593a8b36308c04f8a860c04a7
3vdi <unitcomics.world@proton.me>
2023-10-02 09:27:15 +0530

    installed some dependecies needed

--- src/app/components/MovieCard.js
+++ src/app/components/MovieCard.js
@@ -1,6 +1,6 @@
 export default function MovieCard({ movie }) {
     return (
-        <div class="max-w-[12rem] m-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:border-[2px]">
+        <div class="grid-col max-w-[12rem] m-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:border-[2px]">
 
             <a href="#">
                 <img class="rounded-lg" src={movie.image} width={'250px'} alt="" />
